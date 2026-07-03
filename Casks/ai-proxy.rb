468405e4ef3d1fe76db2e82e0d6320b461d11f54
Hesham <hesham.h.96@gmail.com>
2026-07-03 22:51:02 +0300

    cask: ai-proxy 1.10.1

--- Casks/ai-proxy.rb
+++ Casks/ai-proxy.rb
@@ -1,6 +1,6 @@
 cask "ai-proxy" do
-  version "1.10.0"
-  sha256 "e1f9507f4d8eaef16babb0b8d57982a16eed3c45a742cf5b885764ca6607f262"
+  version "1.10.1"
+  sha256 "06a29d2c5b55fa69473a1880bbb1d8479a8e87dc816628f36b4a43816913f091"
 
   url "https://github.com/heshamh96/homebrew-tap/releases/download/v#{version}/AI-Proxy.zip"
   name "AI-Proxy"
